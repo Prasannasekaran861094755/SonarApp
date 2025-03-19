21be7039b544e253781345c83060815012078c90
NaveenKumar Mathaiyan <naveenkumar.mathaiyan@acsys.com>
2025-03-19 13:14:05 +0530

    added by naveen

--- Views/Home.aspx.cs
+++ Views/Home.aspx.cs
@@ -9,10 +9,9 @@ namespace SonarApp.Views
 {
     public partial class Home : System.Web.UI.Page
     {
-readonly string test="1234";
+ string test="1234";
         protected void Page_Load(object sender, EventArgs e)
         {
-textlabel.Text=test;
         }
     }
 }
